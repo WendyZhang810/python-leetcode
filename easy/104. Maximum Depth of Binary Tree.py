ffc9ee5db38ac0a8652569daff66eb0edd85b6ce
WendyZhang <wendyzhang810@gmail.com>
2020-04-12 03:37:29 +0100

    104. Maximum Depth of Binary Tree
    Signed-off-by: WendyZhang <wendyzhang810@gmail.com>

---
+++ easy/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,66 @@
+'''
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+'''
+#recursion
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        if not root.left and not root.right: return 1
+        else:
+            return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+
+#BFS
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        length=0
+        cur_level=[root]
+        while cur_level:
+            next_level=[]
+            for node in cur_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            cur_level=next_level
+            length+=1
+        return length
